f66e358b3fad3cb8a13aec76752b6a9a5530f2ce
Pedro Carvalho <pedro.juliogc02@gmail.com>
2025-04-02 20:18:07 -0300

    Passing parameters in the request

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,25 @@
 /* 
-    Aula 03 - Utilizando o fetch()
+    Aula 05 - Passando parâmetros na requisição
 */
 
+async function fetchProducts() {
+  const response = await fetch('http://localhost:3333/products');
+  const data = await response.json();
+  console.log(data);
+}
+
+async function fetchProductsById(id) {
+  const response = await fetch(`http://localhost:3333/products/${id}`);
+  const data = await response.json();
+  console.log(data);
+}
+
+// fetchProducts();
+fetchProductsById('3');
+
+/*
+Exemplo utilizando o fetch com then.
 fetch('http://localhost:3333/products')
   .then((response) => response.json())
   .then((data) => console.log(data));
+*/
